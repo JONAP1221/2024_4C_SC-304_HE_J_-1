4aa56453b2cd631f9f165510e1dc505c9d48548f
Camerún <czuniga70365@ufide.ac.cr>
2024-03-07 20:35:43 -0600

    cambio camerun

--- Twitter/src/twitter/Twitter.java
+++ Twitter/src/twitter/Twitter.java
@@ -1,10 +1,14 @@
 package twitter;
+
+import javax.swing.JOptionPane;
+
 public class Twitter {
 
 
     public static void main(String[] args) {
         
         //hola
-        
+        JOptionPane.showMessageDialog(null, "Hola  muchachos", "Se les quiere",
+                JOptionPane.INFORMATION_MESSAGE);
     }//final del main
 }//final de la clase
