0e02c8b77b53d34b858646c4e644c3ee024adbb5
JONAP1221 <jlopez56057@ufide.ac.cr>
2024-03-16 23:05:13 -0600

    Hola
    Camaron sin cola

--- Twitter/src/twitter/Arbol.java
+++ Twitter/src/twitter/Arbol.java
@@ -1,5 +1,6 @@
-
 package twitter;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -7,8 +8,74 @@ package twitter;
  */
 public class Arbol {
     //atributos de la clase
+
     private NodoArbol root;
     
     //hay que ver
     
+
+    public Arbol() {
+        this.root = null;
+    }
+
+    public boolean esVacio() {
+        if (root == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void agregar() {
+        Post d = new Post();
+        d.setMsj((JOptionPane.
+                showInputDialog(null, "Digite el mensaje:")));
+        NodoArbol nuevo = new NodoArbol();
+        nuevo.setMensaje(d);
+        if (esVacio()) {
+            root = nuevo;
+        } else {
+            agregarNuevo(root, nuevo);
+        }
+    }
+
+    public void agregarNuevo(NodoArbol raiz, NodoArbol nuevo) {
+        if (nuevo.getMensaje().getFecha().equals(raiz.getMensaje().getFecha()) || nuevo.getMensaje().getFecha().isBefore(raiz.getMensaje().getFecha())) {
+            if (raiz.getIzquierdo() == null) {
+                raiz.setIzquierdo(nuevo);
+            } else {
+                agregarNuevo(raiz.getIzquierdo(), nuevo);
+            }
+        } else {
+            if (raiz.getDerecho() == null) {
+                raiz.setDerecho(nuevo);
+            } else {
+                agregarNuevo(raiz.getDerecho(), nuevo);
+            }
+        }
+    }
+
+    public void eliminarTodo() {
+        if (!esVacio()) {
+            root = null;
+        } else {
+            JOptionPane.showMessageDialog(null, "¡No se puede eliminar, árbol vacío!");
+        }
+    }
+
+    public void mostrar() {
+        if (!esVacio()) {
+            inOrdenR(root);
+        } else {
+            JOptionPane.showMessageDialog(null, "¡No se puede mostrar, árbol vacío!");
+        }
+    }
+
+    public void inOrdenR(NodoArbol raiz) {
+        if (raiz != null) {
+            inOrdenR(raiz.getIzquierdo());
+            System.out.print( raiz.getMensaje() + "  ");
+            inOrdenR(raiz.getDerecho());
+        }
+    }
 }//Final de la clase
--- Twitter/src/twitter/NodoArbol.java
+++ Twitter/src/twitter/NodoArbol.java
@@ -1,10 +1,10 @@
-
 package twitter;
 
 /**
  *
  * @author Jonathan
  */
+
 public class NodoArbol {
     //atributos de la clase
     private Post mensaje;
@@ -19,4 +19,30 @@ public class NodoArbol {
     //hay que ver
     
     
+    }
+
+    public Post getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(Post elemento) {
+        this.mensaje = elemento;
+    }
+
+    public NodoArbol getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(NodoArbol izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public NodoArbol getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(NodoArbol derecho) {
+        this.derecho = derecho;
+    }
+
 }//final de la clase
