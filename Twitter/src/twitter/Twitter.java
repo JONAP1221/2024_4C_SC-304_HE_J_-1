6f59b550b1efc9cde4359abc2964a4618d062711
Vargas0421 <136260759+Vargas0421@users.noreply.github.com>
2024-04-01 13:00:29 -0600

    actualizacion documentada
    actualizacion documentada

--- Twitter/src/twitter/Twitter.java
+++ Twitter/src/twitter/Twitter.java
@@ -10,7 +10,7 @@ public class Twitter {
 
         boolean continuar = true;
         ListaDobleCircular usuarios = new ListaDobleCircular();
-        ListaSimple seguidores = new ListaSimple();
+        ListaSimple seguidores = new ListaSimple();// se crean 3 ususarios para probar 
         usuarios.insertaMejorado(new Usuario("aaa", "Aaa", 14));
         usuarios.insertaMejorado(new Usuario("bbb", "Bbb", 12));
         usuarios.insertaMejorado(new Usuario("ccc", "Ccc", 1416));
@@ -20,7 +20,7 @@ public class Twitter {
         //try {
         while (continuar) {
             String a;
-            a = JOptionPane.showInputDialog("Opciones de ingreso \n" + "1: agregar usuario \n" + "2: agregar seguidor \n" + "3: ver seguidores de un usuario \n" + "4: crear y enviar post \n" + "5: Mostrar post de usuario \n" + "9: ver usuarios con los id \n" + "0: salir \n");
+            a = JOptionPane.showInputDialog("Opciones de ingreso \n" + "1: agregar usuario \n" + "2: agregar seguidor \n" + "3: ver seguidores de un usuario \n" + "4: crear y enviar post \n" + "5: Mostrar post de usuario \n" + "6: Mostrar el feed de un usuario \n" + "9: ver usuarios con los id \n" + "0: salir \n");
             if (a != null && !a.isEmpty()) { // Verificar si la cadena no está vacía
                 int numero = Integer.parseInt(a);
 
@@ -79,34 +79,30 @@ public class Twitter {
                         break;
 
                     case 4:
-                        String correoDestino = JOptionPane.showInputDialog("Ingrese el correo del usuario al que desea enviar un post:");
-                        Usuario usuarioDestino = usuarios.buscarUsuarioPorCorreo(correoDestino);
-                        if (usuarioDestino != null) {
-                            String mensajePost = JOptionPane.showInputDialog("Ingrese el mensaje del post:");
-                            LocalDate fechaActual = LocalDate.now();
-                            Post nuevoPost = new Post(mensajePost, usuarioDestino, fechaActual);
-
-                            // Apilar el nuevo post en la pila, incluyendo el mensaje
-                            usuarioDestino.getPilaPosts().apilar(nuevoPost, mensajePost);
-
-                            JOptionPane.showMessageDialog(null, "Post creado y enviado a " + correoDestino);
-                        } else {
+                        String correoDestino = JOptionPane.showInputDialog("Ingrese el correo del usuario bajo el cual desea subir un post:");// se pide el ususario
+                        Usuario usuarioDestino = usuarios.buscarUsuarioPorCorreo(correoDestino);// se busca por correo 
+                        if (usuarioDestino != null) {// si el ususario existe 
+                            String mensajePost = JOptionPane.showInputDialog("Ingrese el mensaje del post:");// se pide el texto del post 
+                            LocalDate fechaActual = LocalDate.now();// se toma la fecha del momento a la hora de publicar el post 
+                            Post nuevoPost = new Post(mensajePost, usuarioDestino, fechaActual);// se crea un nuevo post
+                            usuarioDestino.getPilaPosts().apilar(nuevoPost, mensajePost);// se apila el post 
+                            JOptionPane.showMessageDialog(null, "Post creado y publicado bajo el correo: " + correoDestino);// se muetra que se creo el post bajo el destino 
+                        } else {// en caso de no haber ningun correo 
                             JOptionPane.showMessageDialog(null, "No se encontró ningún usuario con el correo electrónico " + correoDestino);
                         }
                         break;
-                    // En el case 5
                     case 5:
-                        String correoUsuarioa = JOptionPane.showInputDialog("Ingrese el correo del usuario del cual desea ver los posts:");
-                        Usuario usuarioa = usuarios.buscarUsuarioPorCorreo(correoUsuarioa);
-                        if (usuarioa != null) {
-                            Pila pilaPosts = usuarioa.getPilaPosts();
-                            if (!pilaPosts.esVacia()) {
-                                System.out.println("Posts de " + usuarioa.getName() + ":");
-                                pilaPosts.mostrarPilaConMensajes(); // Método para mostrar la pila con mensajes
+                        String correoUsuarioa = JOptionPane.showInputDialog("Ingrese el correo del usuario del cual desea ver los posts:");// se pide el correo del ususario 
+                        Usuario usuarioa = usuarios.buscarUsuarioPorCorreo(correoUsuarioa);// se busca el ususario en la lista doble
+                        if (usuarioa != null) {// de haberlo
+                            Pila pilaPosts = usuarioa.getPilaPosts();// de la pila se saca un post
+                            if (!pilaPosts.esVacia()) {// si hay algo en ese ndood // es un tipo de metodo recursivo
+                                System.out.println("Posts de " + usuarioa.getName() + ":");//se muestran los post
+                                pilaPosts.mostrarPilaConMensajes(); // se muestran los post 
                             } else {
-                                System.out.println("El usuario no tiene posts.");
+                                System.out.println("El usuario no tiene posts.");// en caso de == null ser verdad se notifica 
                             }
-                        } else {
+                        } else {// en caso de que no se encontrase un correo 
                             System.out.println("No se encontró ningún usuario con el correo electrónico " + correoUsuarioa);
                         }
                         break;
@@ -121,39 +117,37 @@ public class Twitter {
                                 Post postUsuario = pilaPostsUsuario.desapilar();// se anade al array por medio del metodo desapilar 
                                 allPosts.add(postUsuario);// se anade el post a el array
                             }// como ya tenemos los pots del ususario pasamos con los de las cuentas que este sigue 
-                            ListaSimple seguidoresUsuarioConsulta = usuarioConsulta.getSeguidores();
-                            if (seguidoresUsuarioConsulta != null) {
+                            ListaSimple seguidoresUsuarioConsulta = usuarioConsulta.getSeguidores();// de la lista simple del ussuario se saca un seguidor 
+                            if (seguidoresUsuarioConsulta != null) {// en caso de haberlos y no estar vacia 
                                 NodoListaSimple nodoSeguidor = seguidoresUsuarioConsulta.getCabeza();
-                                while (nodoSeguidor != null) {
-                                    Usuario seguidor = nodoSeguidor.getUsuario();
-                                    Pila pilaPostsSeguidor = seguidor.getPilaPosts();
-                                    while (!pilaPostsSeguidor.esVacia()) {
-                                        Post postSeguidor = pilaPostsSeguidor.desapilar();
-                                        allPosts.add(postSeguidor);
+                                while (nodoSeguidor != null) {// se crea un siclo que recorre hasta estar vacia 
+                                    Usuario seguidor = nodoSeguidor.getUsuario();// se crea una variale tipo ususario apra almacenar el seguidor 
+                                    Pila pilaPostsSeguidor = seguidor.getPilaPosts();// de la pila de ese usuario sacamos los post
+                                    while (!pilaPostsSeguidor.esVacia()) {// si la pila de este ususario no esa vacia 
+                                        Post postSeguidor = pilaPostsSeguidor.desapilar();// se desapila 
+                                        allPosts.add(postSeguidor);// se anade al aaray 
                                     }
-                                    nodoSeguidor = nodoSeguidor.getSiguiente();
+                                    nodoSeguidor = nodoSeguidor.getSiguiente();// se vanza al sigueinte seguidor 
                                 }
                             }
-
-                            // Imprimir los posts
-                            if (!allPosts.isEmpty()) {
-                                System.out.println("Posts de " + usuarioConsulta.getName() + " y usuarios seguidos:");
-                                for (Post post : allPosts) {
-                                    System.out.println(post);
+                            if (!allPosts.isEmpty()) {// si el array no esta vacio 
+                                System.out.println("Posts de " + usuarioConsulta.getName() + " y usuarios seguidos:");// el feed del ususario 
+                                for (Post post : allPosts) {// se recorren el array
+                                    System.out.println(post);// se imprime
                                 }
-                            } else {
-                                System.out.println("No hay posts para mostrar.");
+                            } else {// en caso de que no hayan post en el array 
+                                System.out.println("No hay posts para mostrar.");// se notifica 
                             }
-                        } else {
+                        } else {// no hay ningun usuario con ese correo 
                             JOptionPane.showMessageDialog(null, "No se encontró ningún usuario con el correo electrónico " + correoUsuarioConsulta);
                         }
                         break;
 
                     case 9:
-                        JOptionPane.showConfirmDialog(null, usuarios);
+                        JOptionPane.showConfirmDialog(null, usuarios);// se miestran los usuarios creados 
                         break;
                     case 0:
-                        continuar = false;
+                        continuar = false;// se sale del menu 
                         break;
                     default:
                         System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
