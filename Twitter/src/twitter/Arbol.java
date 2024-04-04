68aca28743cbd6565f228bff5f484b2bd796b55d
JONAP1221 <jlopez56057@ufide.ac.cr>
2024-04-04 17:51:47 -0600

    Arreglo de metodo

--- Twitter/src/twitter/Arbol.java
+++ Twitter/src/twitter/Arbol.java
@@ -1,6 +1,7 @@
 package twitter;
 
 import javax.swing.JOptionPane;
+import twitter.NodoArbol;
 
 /**
  *
@@ -29,15 +30,13 @@ public class Arbol {
     }//Final metodo esVacio
 
     public void agregar(Post mensaje) {
-        
         //seguir comentando esta shit zzz
-        
         NodoArbol nuevo = new NodoArbol(mensaje); //se establece una variable para el nuevo mensaje (Post)
         if (esVacio()) {//si el arbol esta vacio
             root = nuevo; // se crea el Mensaje principal
         }//final if 
         else { // si no (Estas son las respuestas)
-            nuevo.getMensaje().setMsj("      "+nuevo.getMensaje().getMsj());
+            nuevo.getMensaje().setMsj("      " + nuevo.getMensaje().getMsj());
             NodoArbol aux = root; //este esta conectado al mensaje principal
             NodoArbol padre; // otra variable de apoyo para la respuesta a la respuesta
             while (true) {
@@ -59,6 +58,30 @@ public class Arbol {
             }//final while
         }//final else
     }//final metodo agregar
+
+    public void responder(Post mensaje, Arbol post) {
+        NodoArbol nuevo = new NodoArbol(mensaje); //se establece una variable para la respuesta (Post)
+        nuevo.getMensaje().setMsj("      " + nuevo.getMensaje().getMsj());
+        NodoArbol aux = post.getRoot(); //este esta conectado al mensaje principal
+        NodoArbol padre; // otra variable de apoyo para la respuesta a la respuesta
+        while (true) {
+            padre = aux;  //igualamos ambas variables
+            if (padre.getIzquierdo() == null) {
+                aux = aux.getIzquierdo();
+                if (aux == null) {
+                    padre.setIzquierdo(nuevo);
+                    return;
+                }//Final if
+            }//final if
+            else {
+                aux = aux.getDerecho();
+                if (aux == null) {
+                    padre.setDerecho(nuevo);
+                    return;
+                }//final if
+            }//final else
+        }//final while
+    }//final else
 
     public void mostrar() {
         if (!esVacio()) { //si no es vacio
