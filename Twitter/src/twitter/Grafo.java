bd917df09af1146d09ca7dab66c2dd1db2d804d2
Vargas0421 <136260759+Vargas0421@users.noreply.github.com>
2024-04-16 13:48:40 -0600

    comentarios
    comentarios de los metodos

--- Twitter/src/twitter/Grafo.java
+++ Twitter/src/twitter/Grafo.java
@@ -5,32 +5,31 @@ import static twitter.Twitter.usuarios;
 
 public class Grafo {
 
-    public void agregarUsusario() {
+     public void agregarUsusario() {// se se desea crear un ususario se instacia el mismo medinate preguntas para obtener los datos 
         usuarios.insertaMejorado(new Usuario(JOptionPane.showInputDialog("Ingrese su Correo"), JOptionPane.showInputDialog("Ingrese su nombre"), Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad"))));
     }
 
-    public void eliminarUsuario() {
-        String correoUsuarioBase = JOptionPane.showInputDialog("Ingrese el correo de su usuario");
-        Usuario usuarioBase = usuarios.buscarUsuarioPorCorreo(correoUsuarioBase);
+    public void eliminarUsuario() {// para eiliminar un ususario de la lista doble circ 
+        String correoUsuarioBase = JOptionPane.showInputDialog("Ingrese el correo de su usuario");// se pide el correo del ususario 
+        Usuario usuarioBase = usuarios.buscarUsuarioPorCorreo(correoUsuarioBase);// se busca en la lista doble mediante la funcion 
 
-        if (usuarios.existe(usuarioBase)) {
-            usuarios.eliminar(usuarioBase);
-            JOptionPane.showMessageDialog(null, "El ususario con el correo: " + usuarioBase + "se ha eliminado.");
-        } else {
-            JOptionPane.showMessageDialog(null, "El ususario con el correo: " + usuarioBase + "no existe.");
-
+        if (usuarios.existe(usuarioBase)) {// si el usuario existe en la lista doble 
+            usuarios.eliminar(usuarioBase);// se elimina el usuario de la lista doble 
+            JOptionPane.showMessageDialog(null, "El ususario con el correo: " + usuarioBase + "se ha eliminado.");// se muestra el mensaje que el ussuario ha sido eilimnado nunto a el usuario 
+        } else {// de no encontrarse al ussuario
+            JOptionPane.showMessageDialog(null, "El ususario con el correo: " + usuarioBase + "no existe.");// se miestra que el ususario no exitse junto a dicho ususario 
         }
     }
-
-    public void cambiarNombre() {
-        String correoUsuarioBase = JOptionPane.showInputDialog("Ingrese el correo de su usuario");
-        Usuario usuarioBase = usuarios.buscarUsuarioPorCorreo(correoUsuarioBase);
-        usuarios.modificaNombre(usuarioBase);
+    
+     public void cambiarNombre() {// funcion para cambiar nombre
+        String correoUsuarioBase = JOptionPane.showInputDialog("Ingrese el correo de su usuario");// se pide el correo del usuario a modificar 
+        Usuario usuarioBase = usuarios.buscarUsuarioPorCorreo(correoUsuarioBase);// se busca a el ususario medinate la funcion 
+        usuarios.modificaNombre(usuarioBase);// se modifica el nombre
     }
 
-    public void cambiarEdad() {
-        String correoUsuarioBase = JOptionPane.showInputDialog("Ingrese el correo de su usuario");
-        Usuario usuarioBase = usuarios.buscarUsuarioPorCorreo(correoUsuarioBase);
-        usuarios.modificaEdad(usuarioBase);
+    public void cambiarEdad() {// funcino para cambiar la edad
+        String correoUsuarioBase = JOptionPane.showInputDialog("Ingrese el correo de su usuario");// se pide el correo del usuario a modificar 
+        Usuario usuarioBase = usuarios.buscarUsuarioPorCorreo(correoUsuarioBase);// se busca a el ususario medinate la funcion 
+        usuarios.modificaEdad(usuarioBase);// se modifica la edad
     }
 }
--- Twitter/src/twitter/ListaSimple.java
+++ Twitter/src/twitter/ListaSimple.java
@@ -7,75 +7,71 @@ public class ListaSimple {
    
 
     public void insertarSeguidor(Usuario u) {
-        // 1. La lista está vacía.
-        if (cabeza == null) {
-            cabeza = new NodoListaSimple(u);
-        } else {
-            // 2. Se añade algo antes de la cabeza.
-            if (u.getHash() < cabeza.getUsuario().getHash()) {
-                NodoListaSimple nuevoNodo = new NodoListaSimple(u);
-                nuevoNodo.setSiguiente(cabeza);
+        if (cabeza == null) {// en caos de que la lista esta vacia 
+            cabeza = new NodoListaSimple(u);//se crea el primer nodo como la cabeza
+        } else {// en caso de querer meter algo antes que la cabeza
+            if (u.getHash() < cabeza.getUsuario().getHash()) {// se verifica que sea el caso mediante la comparacion de lo hash
+                NodoListaSimple nuevoNodo = new NodoListaSimple(u);// se settea que el nuevo nodo pasa a aser la cabeza 
+                nuevoNodo.setSiguiente(cabeza);// se rfresca la referencia del nodo
                 cabeza = nuevoNodo;
-            } else {
-                // 3. Insertar después de la cabeza.
-                if (cabeza.getSiguiente() == null) {
-                    NodoListaSimple nuevo = new NodoListaSimple(u);
-                    cabeza.setSiguiente(nuevo);
-                } else {
-                    // 4. Insertar al final o en medio.
-                    NodoListaSimple actual = cabeza;
-                    while (actual.getSiguiente() != null && actual.getSiguiente().getUsuario().getHash() < u.getHash()) {
-                        actual = actual.getSiguiente();
+            } else {// en caso de inssertar despues de la cabeza
+                if (cabeza.getSiguiente() == null) {// en caso de que solo este la cabeza
+                    NodoListaSimple nuevo = new NodoListaSimple(u);// se crea la referencia del nodo
+                    cabeza.setSiguiente(nuevo);// se coloca el nuevo nodo justo despues de la cabeza
+                } else {// en caos de necesitar insertar en el centro 
+                    NodoListaSimple actual = cabeza;// se crea un nodo para empezar por la cabeza 
+                    while (actual.getSiguiente() != null && actual.getSiguiente().getUsuario().getHash() < u.getHash()) {// si se esta en la posicion corecta 
+                        actual = actual.getSiguiente();// se pide la referencia del actual al siguiente 
                     }
-                    NodoListaSimple nuevoNodo = new NodoListaSimple(u);
-                    nuevoNodo.setSiguiente(actual.getSiguiente());
-                    actual.setSiguiente(nuevoNodo);
+                    NodoListaSimple nuevoNodo = new NodoListaSimple(u);// se cre un nuevo nodo 
+                    nuevoNodo.setSiguiente(actual.getSiguiente());//se actualiza la informacino del nuevo nodo a la nueva informacion 
+                    actual.setSiguiente(nuevoNodo);// se setea como el nuevo siguiente del nodo menor a donde queremos insertarlo 
                 }
             }
         }
     }
 
-    public void eliminarSeguidor(Usuario u) {
-        NodoListaSimple actual = cabeza;
-        NodoListaSimple anterior = null;
-        while (actual != null) {
-            if (actual.getUsuario() == u) {
-                if (actual == cabeza) {
-                    cabeza = cabeza.getSiguiente();
-                    actual.setSiguiente(null);
-                    break;
-                } else {
-                    anterior.setSiguiente(actual.getSiguiente());
-                    actual.setSiguiente(null);
-                    break;
+    public void eliminarSeguidor(Usuario u) {// funcino para eliminar un seguidor de la lista simple de cada ususario 
+        NodoListaSimple actual = cabeza;// se crea una referencia como cabeza
+        NodoListaSimple anterior = null;// se crea una referencia como null para eliminar el espacion
+        while (actual != null) {// si se tiene algo osea si se sigue a alguien 
+            if (actual.getUsuario() == u) {// se busca el usuario
+                if (actual == cabeza) {// si el que se borra es la cabeza 
+                    cabeza = cabeza.getSiguiente();// se refresca la cabeza al sisguiente seguidor 
+                    actual.setSiguiente(null);// se elimina la referencia del que fue la cabeza
+                    break;// se sale de la funcion 
+                } else {// en caso de no ser la cabeza
+                    anterior.setSiguiente(actual.getSiguiente());// nos bincamos el nodo a eliinar
+                    actual.setSiguiente(null);// se eilimina la referencia del siguienrte del nodo eliminado 
+                    break;// se detiene la ejecucuion 
                 }
-            } else {
+            } else {// en caso de no seysar donde se queire 
                 anterior = actual;
-                actual = actual.getSiguiente();
+                actual = actual.getSiguiente();// se avanza al sisguiente nodo de la lista 
             }
         }
     }
 
-    public boolean existeSeguidor(Usuario usuario) {
-        NodoListaSimple actual = cabeza;
-        while (actual != null) {
-            if (actual.getUsuario().getHash() == usuario.getHash()) {
-                return true; // El usuario es seguidor
+    public boolean existeSeguidor(Usuario usuario) {// funcion para ver si un seguir existe en la lsita simple de cada ususario 
+        NodoListaSimple actual = cabeza;// se empiza por la cabeza mediante un puntero tipo nodo 
+        while (actual != null) {// si hay informacino y no esta vacia 
+            if (actual.getUsuario().getHash() == usuario.getHash()) {// y si se esta en la posicion correcta osea se encontro al ususario 
+                return true; // se devuelve un 1 o sea si se encontro al ussuario deseado 
             }
-            actual = actual.getSiguiente();
+            actual = actual.getSiguiente();// se avanza en caso de no estar donde se requiere 
         }
-        return false; // El usuario no es seguidor
+        return false; // en caso de llegar al final de la lista y no encontrar al ussuario se regresa 0 o false 
     }
     
-    public boolean noExisteSeguidor(Usuario usuario) {
-        NodoListaSimple actual = cabeza;
-        while (actual != null) {
-            if (actual.getUsuario().getHash() == usuario.getHash()) {
-                return false; // El usuario es seguidor
+    public boolean noExisteSeguidor(Usuario usuario) {// funcino que verifica que el seguidor no este 
+        NodoListaSimple actual = cabeza;//se empiza por la cabeza mediante un puntero tipo nodo 
+        while (actual != null) {// si hay informacino y no esta vacia 
+            if (actual.getUsuario().getHash() == usuario.getHash()) {// y si se esta en la posicion correcta osea se encontro al ususario 
+                return false; //si se encuentra al ussuario se devuleve un 0 osea si se sigue 
             }
-            actual = actual.getSiguiente();
+            actual = actual.getSiguiente();// se avanza en la lista para visitarla por completo
         }
-        return true; // El usuario no es seguidor
+        return true; // en caso de que n olo siga osea que no se encunentre en la lista se devuelve true que es lo que se busca como un 1
     }
     
 
